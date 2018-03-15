fe3eb4803fef9dd9498baf72b2b7b2337367f39a
JonasSchmidtler <jonas.schmidtler@tum.de>
2018-03-15 16:21:33 +0100

    TabBar geht irgendwie

--- js/script.js
+++ js/script.js
@@ -4,20 +4,19 @@ function switchChannel(channelName) {
     console.log('tuning into channel', channelName);
     document.getElementById('chat-name').innerHTML=channelName;
     document.getElementById('chat-location').innerHTML='upgrading.never.helps';
-}
-
-function select() {
-    $('li').addClass=('selected');
-}
-
-function appBarStar() {
     $('#starred').attr('src','http://ip.lfe.mw.tum.de/sections/star-o.png');
-   }
+    $('ul li').addClass=('li.selected');
+}
+
+
 
 function appBarStarfill() {
     $('#starred').attr('src','http://ip.lfe.mw.tum.de/sections/star.png');
 }
 
-function selectTab() {
-    
+function selectTab(selectedButton) {
+    $( "button" ).removeClass( "selected" );
+    $(selectedButton).addClass("selected");
+    $(selectedButton).addClass("selected");
+    $(selectedButton).addClass("selected");
 }
