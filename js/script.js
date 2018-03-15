f3844d2d9a63a0084db6fc1400f519e541d721ca
JonasSchmidtler <jonas.schmidtler@tum.de>
2018-03-15 10:45:59 +0100

    channelname changes

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 console.log('app is live')
 
 function switchChannel(channelName) {
-    
-    console.log('tuning into channel', channelName)
+    console.log('tuning into channel', channelName);
+    document.getElementById('chat-name').innerHTML=channelName;
 }
