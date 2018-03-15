797db34b1a9e4ed6e7538cf808ce95354e1b77a3
JonasSchmidtler <jonas.schmidtler@tum.de>
2018-03-15 17:10:56 +0100

    channels selected funktioniert und sterne auch

--- js/script.js
+++ js/script.js
@@ -1,22 +1,23 @@
 console.log('app is live')
 
 function switchChannel(channelName) {
+    $('.selected').removeClass();
     console.log('tuning into channel', channelName);
     document.getElementById('chat-name').innerHTML=channelName;
     document.getElementById('chat-location').innerHTML='upgrading.never.helps';
-    $('#starred').attr('src','http://ip.lfe.mw.tum.de/sections/star-o.png');
-    $('ul li').addClass=('li.selected');
-}
+    $(channelName).addClass('selected');
+    $('.selected').find('.star').attr('src','http://ip.lfe.mw.tum.de/sections/star-o.png');
+};
 
 
 
 function appBarStarfill() {
     $('#starred').attr('src','http://ip.lfe.mw.tum.de/sections/star.png');
-}
+};
 
 function selectTab(selectedButton) {
     $( "button" ).removeClass( "selected" );
     $(selectedButton).addClass("selected");
     $(selectedButton).addClass("selected");
     $(selectedButton).addClass("selected");
-}
+};
