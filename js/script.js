a57f3bca907c3ecc120ff6e75291ee6446471955
JonasSchmidtler <jonas.schmidtler@tum.de>
2018-03-15 10:49:42 +0100

    position location updated

--- js/script.js
+++ js/script.js
@@ -3,4 +3,5 @@ console.log('app is live')
 function switchChannel(channelName) {
     console.log('tuning into channel', channelName);
     document.getElementById('chat-name').innerHTML=channelName;
+    document.getElementById('chat-location').innerHTML='upgrading.never.helps';
 }
