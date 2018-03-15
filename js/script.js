31b4305dbe41cbcc68df11587756673af939dd6f
JonasSchmidtler <jonas.schmidtler@tum.de>
2018-03-15 16:04:00 +0100

    aktueller Stand, keine Sterne gehen, keine Channel Selector gehen, keine Tab Buttons gehen

--- js/script.js
+++ js/script.js
@@ -6,6 +6,18 @@ function switchChannel(channelName) {
     document.getElementById('chat-location').innerHTML='upgrading.never.helps';
 }
 
+function select() {
+    $('li').addClass=('selected');
+}
+
 function appBarStar() {
     $('#starred').attr('src','http://ip.lfe.mw.tum.de/sections/star-o.png');
+   }
+
+function appBarStarfill() {
+    $('#starred').attr('src','http://ip.lfe.mw.tum.de/sections/star.png');
+}
+
+function selectTab() {
+    
 }
